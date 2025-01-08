3209aee15499748c6f16e9f76b194e72502da922
A-yu-sh <ayush.wd.dev@gmail.com>
2025-01-08 14:26:28 +0530

    Added User Input from stdio

--- 03userInput/main.go
+++ 03userInput/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	var welcome string = "Welcome to user input"
 	fmt.Println((welcome))
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name : ")
+
+	// comma ok || error ok
+	// comma ok is used to handle two values at a time
+	// error ok is used to handle one value and one error at a time
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Welcome ", input)
+	fmt.Printf("Input Type : %T",input)
 }
